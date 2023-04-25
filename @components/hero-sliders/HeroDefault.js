81724ba1fab6cfd9add577bbe6110d805a29d0ec
Tony Ramirez <aramirezlecc@adobe.com>
2023-04-25 00:06:31 -0600

    updated

--- @components/hero-sliders/HeroDefault.js
+++ @components/hero-sliders/HeroDefault.js
@@ -11,13 +11,13 @@ export default function HeroDefault() {
         <div className="row align-items-center">
           <div className="col-lg-6 col-md-10 text-center text-lg-start ms-lg-0 me-lg-auto mx-md-auto">
             <h1 className="mb-4 display-3 position-relative z-index-2">Flight School Management Software</h1>
-            <p className="w-lg-75 lead">Scheduling, payments, maintenance, and more.</p>
+            <p className="w-lg-75 lead">Cross platform scheduling, payments, maintenance, and more.</p>
             <p className="mb-5 w-lg-75">
               We have a .5% fee{" "}
               <strong>
                 <em>if</em>
               </strong>{" "}
-              you use our payment processing. Besides that, every other feature{" "}
+              you use our payment processing. Besides that, every other feature is{" "}
               <strong>
                 <em>free</em>
               </strong>
@@ -44,12 +44,12 @@ export default function HeroDefault() {
                 <Image priority layout="responsive" width="690px" height="690px" className="rounded-5" src={HeroStaticBG} alt="" />
               </div>
               {/**Feature Icon */}
-              <div className="bg-white rotate-4 shadow-lg position-absolute end-0 top-0 width-240 h-auto text-dark rounded-4 p-3">
-                <FeatureIconTiny icon={<span className="material-symbols-rounded align-middle fs-3">workspaces</span>} color="warning" text="Easy scheduling" />
+              <div className="bg-white rotate-4 shadow-lg position-absolute end-0 top-0 width-220 h-auto text-dark rounded-4 p-3">
+                <FeatureIconTiny icon={<span className="material-symbols-rounded align-middle fs-3">credit_card</span>} color="warning" text="Payment Processing" />
               </div>
               {/**Feature Icon */}
               <div className="bg-white rotate-n4 shadow-lg position-absolute start-0 bottom-0 width-220 h-auto text-dark rounded-4 p-3">
-                <FeatureIconTiny icon={<span className="material-symbols-rounded align-middle fs-3">task</span>} color="primary" text="Manage your flight program" />
+                <FeatureIconTiny icon={<span className="material-symbols-rounded align-middle fs-3">devices</span>} color="primary" text="iOS, Android, and Web" />
               </div>
             </div>
           </div>
